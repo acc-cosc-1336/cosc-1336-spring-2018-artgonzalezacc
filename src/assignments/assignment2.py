16d0adfbae3ea99363f1e89643e3e53ecd4dbc51
Art Gonzalez <arturo.gonzalez@austincc.edu>
2018-01-26 16:18:40 -0600

    assignment 2 checkin

---
+++ src/assignments/assignment2.py
@@ -0,0 +1,31 @@
+def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
+    '''
+    Write code to calculate faculty evaluation rating according to asssignment instructions
+
+    :param nev: Never
+    :param rar: Rarely
+    :param som: Sometimes
+    :param oft: Often
+    :param voft: Very Often
+    :param alw: Always
+    :return: rating as a string
+    '''
+    total = nev + rar + som + oft + voft + alw
+
+    alw_percent = alw / total * 100
+    voft_percent = voft / total * 100
+    oft_percent = oft / total * 100
+    som_percent = som / total * 100
+    rar_percent = rar / total * 100
+
+    if voft_percent + alw_percent >= 90:
+        return "Excellent"
+    elif oft_percent + voft_percent + alw_percent >= 80:
+        return "Very Good"
+    elif som_percent + oft_percent + voft_percent + alw_percent >= 70:
+        return "Good"
+    elif rar_percent + som_percent + oft_percent + voft_percent + alw_percent >= 60:
+        return "Needs Improvement"
+    else:
+        return "Unacceptable"
+
---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,5 @@
+from src.assignments.assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+result = faculty_evaluation_result(1,1,2,25,50,75)
+print(result)
